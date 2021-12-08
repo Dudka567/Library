d9d632505bf7835a9b879a727b59fd56102fc1f3
Даниил Селянкин <55666999+Dudka567@users.noreply.github.com>
2021-12-08 15:12:16 +0300

    update 3.7

--- startTest.bat
+++ startTest.bat
@@ -1,6 +1,6 @@
-set DIR_PROJECT=C:\\Library\src\test\classes
+set DIR_PROJECT=\Library\src\test\classes
 del /s %DIR_BIN%\*.class >NUL
-cd C:\\Library\src\test\javaFiles
-javac -d C:\\Library\src\test\classes -classpath javac -d C:\\Library\src\test\classes -classpath C:\\Library\src\test\resources\junit-4.13.1.jar *.java
-cd C:\\Library\src\main\classes
-java -classpath C:\\Library\src\main\classes src.main.javaFiles.Main
+cd \Library\src\test\javaFiles
+javac -d \Library\src\test\classes -classpath javac -d \Library\src\test\classes -classpath \Library\src\test\resources\junit-4.13.1.jar *.java
+cd \Library\src\main\classes
+java -classpath \Library\src\main\classes src.main.javaFiles.Main
