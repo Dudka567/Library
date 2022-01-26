2c2c391222b39bff44a5bab3c6f0069cd4fbee22
Даниил Селянкин <55666999+Dudka567@users.noreply.github.com>
2022-01-26 15:18:51 +0300

    deleted otherFiles

--- out/production/Library/startTest.bat
+++
@@ -1,8 +0,0 @@
-set DIR_PROJECT=src/test/classes
-del /s %DIR_BIN%\*.class >NUL
-cd src/test/javaFiles
-javac -d ../classes -classpath ..\resources\junit-4.13.1.jar;..\..\..\ *.java
-cd..
-cd classes
-java -classpath ..\resources\junit-4.13.1.jar; src.test.javaFiles.LibraryTest
-pause
