9ab3c141df68870427f406e6985245476f2f4054
Даниил Селянкин <55666999+Dudka567@users.noreply.github.com>
2022-02-11 14:05:16 +0300

    Init commit
    Fix remarks

--- module/src/main/java/controller/validators/LibraryValidator.java
+++ module/src/main/java/controller/validators/LibraryValidator.java
@@ -3,6 +3,10 @@ package src.main.java.controller.validators;
 import java.util.regex.Pattern;
 
 public class LibraryValidator implements Validator {
+    private static final String INCORRECT_KEY_AND_VALUE_FORMAT = "Incorrect key and value format.";
+    private static final String INCORRECT_KEY_FORMAT = "Incorrect key format.";
+    private static final String INCORRECT_VALUE_FORMAT = "Incorrect value format.";
+    private static final String PAIR_ADDED = "The pair has been added to the dictionary.";
     private Pattern patternKey;
     private Pattern patternValue;
 
@@ -13,7 +17,17 @@ public class LibraryValidator implements Validator {
 
     @Override
     public ValidationResult validatePair(String key, String value) {
-        ValidationResult validationResult = new ValidationResult(patternKey.matcher(key).matches(), patternValue.matcher(value).matches());
+        boolean boolResultValidation = false;
+        ValidationResult validationResult;
+        if (boolResultValidation = (patternKey.matcher(key).matches() && patternValue.matcher(value).matches())) {
+            validationResult = new ValidationResult(PAIR_ADDED, boolResultValidation);
+        } else if (boolResultValidation = (!patternKey.matcher(key).matches() && patternValue.matcher(value).matches())) {
+            validationResult = new ValidationResult(INCORRECT_KEY_AND_VALUE_FORMAT, boolResultValidation);
+        } else if (boolResultValidation = (!patternKey.matcher(key).matches())) {
+            validationResult = new ValidationResult(INCORRECT_KEY_FORMAT, boolResultValidation);
+        } else {
+            validationResult = new ValidationResult(INCORRECT_VALUE_FORMAT, boolResultValidation);
+        }
         return validationResult;
     }
 }
--- module/src/main/java/controller/validators/ValidationResult.java
+++ module/src/main/java/controller/validators/ValidationResult.java
@@ -1,10 +1,6 @@
 package src.main.java.controller.validators;
 
 public class ValidationResult {
-    private static final String INCORRECT_KEY_AND_VALUE_FORMAT = "Incorrect key and value format.";
-    private static final String INCORRECT_KEY_FORMAT = "Incorrect key format.";
-    private static final String INCORRECT_VALUE_FORMAT = "Incorrect value format.";
-    private static final String PAIR_ADDED = "The pair has been added to the dictionary.";
     private String stringValidationResult;
     private Boolean isValidationResult;
 
@@ -16,28 +12,9 @@ public class ValidationResult {
         return isValidationResult;
     }
 
-    public ValidationResult(Boolean isKey, Boolean isValue) {
-        this.stringValidationResult = getStringResultValidation(isKey, isValue);
-        this.isValidationResult = getBooleanResultValidation(isKey, isValue);
+    public ValidationResult(String stringValidationResult, Boolean isValidationResult) {
+        this.stringValidationResult = stringValidationResult;
+        this.isValidationResult = isValidationResult;
     }
 
-    private String getStringResultValidation(Boolean isKey, Boolean isValue) {
-        if (isKey && isValue) {
-            return PAIR_ADDED;
-        } else if (!isKey && !isValue) {
-            return INCORRECT_KEY_AND_VALUE_FORMAT;
-        } else if (!isKey) {
-            return INCORRECT_KEY_FORMAT;
-        } else {
-            return INCORRECT_VALUE_FORMAT;
-        }
-    }
-
-    private Boolean getBooleanResultValidation(Boolean isKey, Boolean isValue) {
-        if (isKey && isValue) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 }
